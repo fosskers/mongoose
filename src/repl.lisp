107da07321b0e4a056dd8999d7f8ef997601f34b
Colin Woodbury <colin@fosskers.ca>
2026-01-11 08:16:44 +0900

    [repl] basic profiling

--- src/repl.lisp
+++ src/repl.lisp
@@ -1,11 +1,13 @@
 (in-package :mongoose)
 
 (defparameter +content-type-html+ (format nil "Content-Type: text/html; charset=utf-8~c~%" #\return))
+(defparameter *requests* 0)
 
 ;; NOTE: 2026-01-06 `define-alien-callable' is very powerful! Even when its
 ;; associated `alien-callable-function' is actually being used as a callback in
 ;; a live loop (see below), I can still edit this Lisp, recompile it, and the
 ;; changes will be reflected immediately in subsequent requests.
+#+sbcl
 (define-alien-callable ev-handler void ((c (* connection)) (ev int) (ev-data (* t)))
   "Handle HTTP events."
   (when (= ev +ev-http-msg+)
@@ -15,6 +17,7 @@
       ;; (format t "METHOD: '~a'~%" (str->lisp (slot hm 'method)))
       ;; (format t "URI: '~a'~%" uri)
       ;; (format t "QUERY: '~a'~%" (str->lisp (slot hm 'query)))
+      (incf *requests*)
       (cond
         #+nil
         ((string= "/foo" uri)
@@ -49,9 +52,26 @@
   (mgr-free (addr mgr)))
 
 #+nil
+(sb-sprof:with-profiling (:max-samples 100000 :sample-interval 0.001 :report :graph)
+  (with-alien ((mgr (struct mgr)))
+    (setf *requests* 0)
+    (mgr-init (addr mgr))
+    (format t "Establishing handler...~%")
+    (let ((handler (alien-sap (alien-callable-function 'ev-handler))))
+      (http-listen (addr mgr) "http://localhost:8000" handler nil))
+    (format t "Waiting for requests...~%")
+    (loop :while (< *requests* 100000)
+          :do (mgr-poll (addr mgr) 1000))
+    (format t "Exiting~%")
+    (mgr-free (addr mgr))))
+
+#+nil
 (html:html (:raw "<!DOCTYPE html>")
            (:html
             (:head (:meta :charset "utf-8"))
             (:body (:div :id "foo"
                          :class "bar"
                          (:span "hello")))))
+
+#+nil
+(require :sb-sprof)
