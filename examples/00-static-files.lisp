fe9c28422594adc280224b77e0df368b8706c04c
Colin Woodbury <colin@fosskers.ca>
2026-01-20 07:18:15 +0900

    [examples] avoid a crash when serving files

--- examples/00-static-files.lisp
+++ examples/00-static-files.lisp
@@ -1,15 +1,20 @@
 ;;; Serve static files from the filesystem.
+;;;
+;;; Simple enough: tell it what directory to serve, and call `http-serve-dir'.
 
 (in-package :mongoose)
 
 (define-alien-callable ev-handler void ((c (* connection)) (ev int) (ev-data (* t)))
   "Handle HTTP events."
   (when (= ev +ev-http-msg+)
-    ;; NOTE The `ev-data' comes in "naked", so we must do a pointer cast (this
+    ;; NOTE: The `ev-data' comes in "naked", so we must do a pointer cast (this
     ;; is free) to the real type we require.
     (let ((hm   (cast ev-data (* http-message)))
           (opts (make-alien http-serve-opts)))
       (setf (slot opts 'root-dir) ".")
+      ;; NOTE: Notice the need to explicitly set this to NULL. This is a
+      ;; Mongoose setting that ensures Posix will be used as the filesystem.
+      (setf (slot opts 'fs) nil)
       (http-serve-dir c hm opts)
       (free-alien opts))))
 
